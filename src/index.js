b638ba8739145825526383f7b192f40b590ef598
USUI Tomoaki <t.usui@gn-office.pro>
2022-04-18 12:27:21 +0900

    Add unlink method

--- src/index.js
+++ src/index.js
@@ -1,81 +1,107 @@
-const mix = require("laravel-mix")
-const colors = require("colors")
-const fs = require("fs")
-const isSymbolicLink = require("is-symbolic-link")
-const resolve = require("path").resolve
-
+const mix = require('laravel-mix')
+const colors = require('colors')
+const fs = require('fs')
+const isSymbolicLink = require('is-symbolic-link')
+const resolve = require('path').resolve
 
 class SymbolicFolder {
-    register(target, path) {
-		
-		target = resolve(target)
-        
-		if(!fs.existsSync(path)){		
-			//We know path doesn't exist. Attempt creating the symbolic link
-			
-			try {
-				fs.symlinkSync( target, path, "dir" )
-				if(fs.statSync(path).isDirectory()){
-					//Success
-					console.log(`> Symbolic link at '${path}' created successfully`.bold.green)
+  register (target, path) {
 
-				} else {
-					//Fail
-					console.log(`> The symbolic link at '${path}' failed to be created correctly`.bold.red)
-				}
-				
-			} catch(err){
-				//Error
-				console.log(`> The symbolic link at '${path}' failed to be created correctly \n\n ${err}`.bold.red)
-			}
-			
-		} else {
-			//If the existing file is NOT a symbolic link, alert the user
-			if(!isSymbolicLink.sync(path)){
-				console.log(`> The symbolic link at '${path}' could not be created. Another file with this name already exists`.bold.red)
-			}
-			//Else, it's a symbolic link... do nothing
-			
-		}
+    target = resolve(target)
+
+    if (!fs.existsSync(path)) {
+      //We know path doesn't exist. Attempt creating the symbolic link
+
+      try {
+        fs.symlinkSync(target, path, 'dir')
+        if (fs.statSync(path).isDirectory()) {
+          //Success
+          console.log(
+            `> Symbolic link at '${path}' created successfully`.bold.green)
+
+        } else {
+          //Fail
+          console.log(
+            `> The symbolic link at '${path}' failed to be created correctly`.bold.red)
+        }
+
+      } catch (err) {
+        //Error
+        console.log(
+          `> The symbolic link at '${path}' failed to be created correctly \n\n ${err}`.bold.red)
+      }
+
+    } else {
+      //If the existing file is NOT a symbolic link, alert the user
+      if (!isSymbolicLink.sync(path)) {
+        console.log(
+          `> The symbolic link at '${path}' could not be created. Another file with this name already exists`.bold.red)
+      }
+      //Else, it's a symbolic link... do nothing
+
     }
+  }
 }
-//NOP
 
 class SymbolicFile {
-    register(target, path) {
-		
-		target = resolve(target)
-        
-		if(!fs.existsSync(path)){		
-			//We know path doesn't exist. Attempt creating the symbolic link
-			
-			try {
-				fs.symlinkSync( target, path )
-				if(fs.statSync(path).isFile()){
-					//Success
-					console.log(`> Symbolic link at '${path}' created successfully`.bold.green)
+  register (target, path) {
 
-				} else {
-					//Fail
-					console.log(`> The symbolic link at '${path}' failed to be created correctly`.bold.red)
-				}
-				
-			} catch(err){
-				//Error
-				console.log(`> The symbolic link at '${path}' failed to be created correctly \n\n ${err}`.bold.red)
-			}
-			
-		} else {
-			//If the existing file is NOT a symbolic link, alert the user
-			if(!isSymbolicLink.sync(path)){
-				console.log(`> The symbolic link at '${path}' could not be created. Another file with this name already exists`.bold.red)
-			}
-			//Else, it's a symbolic link... do nothing
-			
-		}
+    target = resolve(target)
+
+    if (!fs.existsSync(path)) {
+      //We know path doesn't exist. Attempt creating the symbolic link
+
+      try {
+        fs.symlinkSync(target, path)
+        if (fs.statSync(path).isFile()) {
+          //Success
+          console.log(
+            `> Symbolic link at '${path}' created successfully`.bold.green)
+
+        } else {
+          //Fail
+          console.log(
+            `> The symbolic link at '${path}' failed to be created correctly`.bold.red)
+        }
+
+      } catch (err) {
+        //Error
+        console.log(
+          `> The symbolic link at '${path}' failed to be created correctly \n\n ${err}`.bold.red)
+      }
+
+    } else {
+      //If the existing file is NOT a symbolic link, alert the user
+      if (!isSymbolicLink.sync(path)) {
+        console.log(
+          `> The symbolic link at '${path}' could not be created. Another file with this name already exists`.bold.red)
+      }
+      //Else, it's a symbolic link... do nothing
+
     }
+  }
 }
 
+class UnlinkSymbolicLink {
+  register (path) {
 
-mix.extend("createSymbolicFolder", new SymbolicFolder());
-mix.extend("createSymbolicFile", new SymbolicFile());
+    // check path is symbolic link
+    if (isSymbolicLink.sync(path)) {
+
+      fs.unlink(path, (err) => {
+        if (err) throw err
+        //Error
+        console.log(
+          `> The symbolic link at '${path}' failed to be deleted correctly \n\n ${err}`.bold.red)
+      })
+
+    } else {
+      console.log(`> '${path}' is not symbolic link`.bold.red)
+
+    }
+  }
+}
+
+mix.extend('createSymbolicFolder', new SymbolicFolder())
+mix.extend('createSymbolicFile', new SymbolicFile())
+mix.extend('unlinkSymbolicFile', new UnlinkSymbolicLink())
